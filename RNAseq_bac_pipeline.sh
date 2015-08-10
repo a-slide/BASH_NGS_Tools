4b37b986ed28051a1f07972ff0a6ddeedc5a9e5c
a-slide <adrien.leger@gmail.com>
2015-08-10 17:28:09 +0200

    add a tool for the Baculo_RNAseq project

---
+++ RNAseq_bac_pipeline.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+if [[ "$#" != 5 ]]
+then
+    echo -e "USAGE: bwa_pipeline_single.sh <bwa_index_path> <sequence.fa.gz> <gff_file> <min_mapq_score> <n_threads>"
+    exit 1
+fi
+
+# Dependencies
+# samtools 1.2+
+# bwa 0.1.12+
+# samstat 1.5+ 
+# bedtools 2.17.0+
+# HTSeq 0.6.1p1+
+
+# Extract basename
+bn=`basename ${2%.fastq.gz}`
+echo -e "PROCESSING SAMPLE: $bn\n"
+
+# Align with bwa mem
+echo -e "Mapping with bwa mem and sort bam\n"
+bwa mem -M -t "$5" "$1" "$2" | samtools view - -hb -@ "$5" | samtools sort - -@ "$5" -o a > "$bn"_raw.bam
+samstat "$bn"_raw.bam
+
+## Filter out unmapped and secondary reads 
+echo -e "Filter_bam\n"
+samtools view "$bn"_raw.bam -hb -F 260 -q "$4" | samtools sort - -@ "$5" -o a > "$bn".bam
+samstat "$bn".bam 
+
+## Index bam and generates bedgraph
+echo -e "\nProcessing with samtools index\n"
+samtools index "$bn".bam
+echo -e "\nCreating bedgraph\n"
+genomeCoverageBed -ibam "$bn".bam -bga -trackline -trackopts "name="$bn" color=250,0,0" > "$bn".bedgraph
+
+# Create HTSeq-Count file
+echo -e "\nCounting reads overlaping features with HTSeq count\n"
+htseq-count -f bam -s no -m union -t exon "$bn".bam "$3" > $bn.counts
+
+exit 0
